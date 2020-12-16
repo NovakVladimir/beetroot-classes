7c3dd7da91cf3cb2be28022b715fef05f6c8e528
NovakVladimir <vladimir.novak.89@i.ua>
2020-12-16 20:40:53 +0200

    Finish lesson

---
+++ js/main.js
@@ -0,0 +1,20 @@
+class Point {
+    constructor() {
+        this.x = 123;
+        this.y = 456
+    }
+}
+
+// let point1 = {
+//     x: 123;
+//     y 456;
+// }
+// console.log(point1);
+
+let point2 = new Point();
+
+class Point3D extends Point {
+    
+}
+
+let point3 = new Point3D(12, 34);
